b1ebd3fc784dbccb366531c32614f23395b761df
JuliaAnel <julija.anjol@gmail.com>
2021-01-25 22:23:03 +0300

    Task2 added

---
+++ src/eu/senla/task2/Main.java
@@ -0,0 +1,14 @@
+package eu.senla.task2;
+
+public class Main {
+    public static void main(String[]args){
+    Rainbow rainbow = new Rainbow();
+        rainbow.number = 4;
+        rainbow.displayInfo();
+        rainbow.number = 7;
+        rainbow.displayInfo();
+        rainbow.number = 13;
+        rainbow.displayInfo();
+    }
+
+}
---
+++ src/eu/senla/task2/Rainbow.java
@@ -0,0 +1,55 @@
+package eu.senla.task2;
+
+public class Rainbow {
+int number;
+void displayInfo(){
+    switch (number){
+        case 1:
+            System.out.println("Red");
+            break;
+        case 2:
+            System.out.println("Orange");
+            break;
+        case 3:
+            System.out.println("Yellow");
+            break;
+        case 4:
+            System.out.println("Green");
+            break;
+        case 5:
+            System.out.println("Blue");
+            break;
+        case 6:
+            System.out.println("Cyan");
+            break;
+        case 7:
+            System.out.println("Purple");
+            break;
+        case 8:
+            System.out.println("Green-blue");
+            break;
+        case 9:
+            System.out.println("Red-Orange");
+            break;
+        case 10:
+            System.out.println("Orange-yellow");
+            break;
+        case 11:
+            System.out.println("Blue-cyan");
+            break;
+        case 12:
+            System.out.println("Cyan-purple");
+            break;
+        case 13:
+            System.out.println("Blue-purple");
+            break;
+
+    }
+}
+
+
+
+}
+
+
+
